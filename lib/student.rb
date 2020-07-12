178ece1852d577004121657f146881916734d571
Dang Nguyen <danghwin@gmail.com>
2020-07-12 14:45:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,10 @@ require_relative "../config/environment.rb"
 
 class Student
   
-  attr_accessor :name, :grade
+  attr_accessor :id,  :name, :grade
 
 def initialize(id = nil,name, grade)
-  @id = nil
+  @id = id
   @name = name
   @grade = grade 
   
