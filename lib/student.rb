cbbee245ca27142f8f6d54060ca127792b935612
Dang Nguyen <danghwin@gmail.com>
2020-07-12 14:49:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,9 +14,9 @@ end
   def self.create_table
     sql = <<- SQL 
     CREATE TABLE students (
-      id INTEGER PRIMARY KEY
-      name, TEXT
-      grade INTEGER 
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER,
       
       
       
